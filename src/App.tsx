4d4334191dc4eaba10df39897c45581202930b1e
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2023-04-24 21:35:03 -0400

    Product Page
    Product page now created. User can now click on an item, and be brought to a product page that dynamically shows information based off the product.

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,45 @@ import Footer from './layouts/footer/Footer';
 import Hero from './layouts/home-page/hero/Hero';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-
-
-
-
-
+import ShopAll from './layouts/shop-all/ShopAll';
+import { useEffect, useState } from 'react';
+import ProductPage from './layouts/product-page/ProductPage'
 function App() {
+const [products, setProducts] = useState()
+  // useEffect(() => {
+  //   const getProducts = async () => {
+
+  //     const url = `http://localhost:3000/products`
+  //     const username = 'myusername';
+  //     const password = 'mypassword';
+  //     const credentials = btoa(`${username}: ${password}`)
+  //     try {
+  //       const response = await fetch('http://localhost:8080/products', {
+  //         headers: {
+  //           'Authorizaiton' : `Basic ${credentials}`,
+  //         },
+  //       });
+  //       if(response.ok) {
+  //         const responseData = await response.json();
+  //         setProducts(responseData._embedded.products)
+  //       } else {
+  //         throw new Error('Request Failed')
+  //       }
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  //   getProducts()
+  //   }, [])
   return (
     <div className='app'>
       <Router>
       <Nav/>
         <Routes>
           <Route path="/" element={<HomePageOutput/>}/>
-          
-      
-
+          <Route path={`/products/:productId`} element={<ProductPage/>}/>
         </Routes>
-      {/* <Footer/> */}
+      <Footer/>
       </Router>
 
     </div>
--- src/layouts/home-page/how-it-works/HowItWorks.tsx
+++ src/layouts/home-page/how-it-works/HowItWorks.tsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import './how-it-works.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIcons, faPaintBrush, faPaintbrush, faPencilAlt, faUsers } from '@fortawesome/free-solid-svg-icons';
 const HowItWorks = () => {
   return (
     <div className='howItWorks'>
       <div className='howItWorks-container'>
         <h6 className='howItWorks-title'>HOW IT WORKS</h6>
-        <p className='howItWorks-title-paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam deleniti, voluptatibus officiis eius corporis quas! Doloribus voluptatum itaque eum laborum!</p>
+        <p className='howItWorks-title-paragraph'>At Matchbox Stickers, we understand the importance of exclusivity and uniqueness in the world of art. That's why we've created a marketplace where creators can set the exact time and date their creations will be available for sale, as well as the limited quantity they wish to offer.</p>
         <div className='howItWorks-image-info-container'>
           <div className='howItWorks-info-container1'>
-            <div className='howItWorks-image1'></div>
-            <div className='howItWorks-imagetitle-styles'>Lorem, ipsum dolor.</div>
-            <div className='howItWorks-imageparagraph-styles'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque iste culpa saepe debitis necessitatibus soluta, exercitationem assumenda officia molestiae eius.</div>
+            <div style={{marginBottom: '15px'}}>
+
+          <FontAwesomeIcon style={{marginBottom: '15px'}} size= '5x' icon={faPaintbrush} />
+            <div className='howItWorks-imagetitle-styles'>Creators</div>
+            </div>
+            <div className='howItWorks-imageparagraph-styles'>Simply upload your one-of-a-kind designs, set your sale parameters, and watch as eager collectors flock to your exclusive, time-sensitive sale. But remember, once they're sold out, they're gone for good, adding to the excitement and anticipation of each new release.</div>
           </div>
           <div className='howItWorks-image-info-container2'>
-            <div className='howItWorks-image2'></div>
-            <div className='howItWorks-imagetitle-styles' >Lorem, ipsum dolor.</div>
-            <div className='howItWorks-imageparagraph-styles'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque iste culpa saepe debitis necessitatibus soluta, exercitationem assumenda officia molestiae eius.</div>
+            <div style={{marginBottom: '15px'}}>
+
+          <FontAwesomeIcon style={{marginBottom: '15px'}} size= '5x' icon={faIcons} />
+            <div className='howItWorks-imagetitle-styles' >Collectors</div>
+            </div>
+            <div className='howItWorks-imageparagraph-styles'>Matchbox Stickers offers a treasure trove of rare and unique designs that can't be found anywhere else.  Plus, with new creations going live regularly, there's always something fresh and exciting to discover.</div>
           </div>
           <div className='howItWorks-image-info-container3'>
-            <div className='howItWorks-image3'></div>
-            <div className='howItWorks-imagetitle-styles' >Lorem, ipsum dolor.</div>
-            <div className='howItWorks-imageparagraph-styles'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque iste culpa saepe debitis necessitatibus soluta, exercitationem assumenda officia molestiae eius.</div>
+            <div style={{marginBottom: '15px'}}>
+
+          <FontAwesomeIcon style={{marginBottom: '15px'}} size= '5x' icon={faUsers} />
+            <div className='howItWorks-imagetitle-styles' >The Community</div>
+            </div>
+            <div className='howItWorks-imageparagraph-styles'>By connecting artists and collectors in a dynamic, fast-paced environment, Matchbox Stickers is revolutionizing the world of limited edition stickers. So, don't wait any longer! Experience the thrill of collecting and creating like never before!</div>
           </div>
         </div>
         <div className='howItWorks-getdropalerts-container'>
@@ -30,7 +41,7 @@ const HowItWorks = () => {
               <div className='howItWorks-getdropalerts-text-container'>
                 <h5>GET DROP ALERTS</h5>
                 <h6>WE PROMISE NOT TO SPAM YOU.</h6>
-                <p>Ipsam, iure molestiae! Et officia laboriosam fugit impedit officiis, ipsum eum quisquam.</p>
+                <p>Sign up for alerts for upcomming drops and new opportunities for creators.</p>
               </div>
               <div className='input-button-container'>
                 <input className='howItWorks-getdropalerts-input' placeholder='    YOUR EMAIL GOES HERE'></input>
