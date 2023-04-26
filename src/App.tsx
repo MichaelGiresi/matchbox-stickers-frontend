1b3144093c9023fb9bbbeb3b9680b6e3c19c514f
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2023-04-25 20:16:28 -0400

    Cart and Checkout Pages
    Cart and Checkout Pages created. Cart is dynamic, and locally stored.

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,98 @@ import './App.css';
 import Nav from './layouts/nav/Nav';
 import HomePageOutput from './layouts/home-page/output/HomePageOutput';
 import Footer from './layouts/footer/Footer';
+import { CartContext } from './contexts/CartContext'
 import Hero from './layouts/home-page/hero/Hero';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-
-
-
-
-
+import ShopAll from './layouts/shop-all/ShopAll';
+import { useEffect, useState } from 'react';
+import ProductPage from './layouts/product-page/ProductPage'
+import Cart from './layouts/cart/Cart';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Checkout from './layouts/checkout/Checkout';
 function App() {
+const [products, setProducts] = useState()
+const [cart, setCart] = useState(false)
+
+const [cartSubTotal, setCartSubTotal] = useState(() => {
+  const cartSubTotal = localStorage.getItem('cartSubTotal');
+  return cartSubTotal ? JSON.parse(cartSubTotal) : 0
+})
+
+const [cartCount, setCartCount] = useState(() => {
+  const storedCartCount = localStorage.getItem('cartCount');
+  return storedCartCount ? JSON.parse(storedCartCount) : 0
+})
+
+const [localCartItems, setLocalCartItems] = useState(() => {
+  const storedCartItems = localStorage.getItem('localCartItems');
+  return storedCartItems ? JSON.parse(storedCartItems) : []
+})
+
+useEffect(() => {
+  localStorage.setItem('cartSubTotal', JSON.stringify(cartSubTotal));
+}, [cartSubTotal]);
+
+useEffect(() => {
+  localStorage.setItem('cartCount', JSON.stringify(cartCount));
+}, [cartCount]);
+
+useEffect(() => {
+  localStorage.setItem('localCartItems', JSON.stringify(localCartItems));
+}, [localCartItems]);
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+
+  //     const url = `http://localhost:3000/products`
+  //     const username = 'myusername';
+  //     const password = 'mypassword';
+  //     const credentials = btoa(`${username}: ${password}`)
+  //     try {
+  //       const response = await fetch('http://localhost:8080/products', {
+  //         headers: {
+  //           'Authorizaiton' : `Basic ${credentials}`,
+  //         },
+  //       });
+  //       if(response.ok) {
+  //         const responseData = await response.json();
+  //         setProducts(responseData._embedded.products)
+  //       } else {
+  //         throw new Error('Request Failed')
+  //       }
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  //   getProducts()
+  //   }, [])
   return (
     <div className='app'>
       <Router>
+      <CartContext.Provider
+          value={{
+            cart,
+            setCart,
+            cartSubTotal,
+            setCartSubTotal,
+            cartCount,
+            setCartCount,
+            localCartItems,
+            setLocalCartItems,
+          }}>
       <Nav/>
         <Routes>
           <Route path="/" element={<HomePageOutput/>}/>
-          
-      
-
+          <Route path={`/products/:productId`} element={<ProductPage/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
-      {/* <Footer/> */}
+      <Footer/>
+      </CartContext.Provider>
       </Router>
-
+      <ToastContainer />
     </div>
   );
 }
--- src/layouts/home-page/previous-drops/PreviousDrops.tsx
+++ src/layouts/home-page/previous-drops/PreviousDrops.tsx
@@ -1,7 +1,129 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './previous-drops.css'
-
+import { Product } from '../../../models/Product'
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../../contexts/CartContext';
+import {toast} from 'react-toastify'
 const PreviousDrops = () => {
+  const cartContext = useContext(CartContext)
+  const [previousDrops, setPreviousDrops] = useState<Product[] | undefined>();
+  const [numContainers, setNumContainers] = useState(3)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const progressPercentage = (currentIndex / ((previousDrops?.length || 1) - numContainers)) * 100;
+  
+useEffect(() => {
+  // console.log(cartContext?.cartCount)
+}, [cartContext?.cartCount])
+
+
+  useEffect(() => {
+    const getPreviousDrops = async () => {
+      const username = 'myusername';
+      const password = 'mypassword';
+      const credentials = btoa(`${username}:${password}`);
+      
+      try {
+        const response = await fetch('http://localhost:8080/products', {
+          headers: {
+            'Authorization' : `Basic ${credentials}`,
+          },
+        });
+  
+        if(response.ok) {
+          const responseData = await response.json();
+          setPreviousDrops(responseData._embedded.products)
+        } else {
+          throw new Error('Request failed')
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getPreviousDrops()
+  }, [])
+
+  useEffect(() => {
+
+    updateNumContainers();
+    window.addEventListener('resize', updateNumContainers);
+
+    return () => {
+      window.removeEventListener('resize', updateNumContainers)
+    }
+  }, [previousDrops])
+
+  const updateNumContainers = () => {
+    if(window.innerWidth <= 768) {
+      setNumContainers(1)
+    } else {
+      setNumContainers(3);
+    }
+  }
+
+  const handleLeftButtonClick = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex > 0) {
+        return prevIndex - 1;
+      } else {
+        // If at the beginning of the array, move to the last valid index
+        const lastIndex = Math.max((previousDrops?.length || 1) - numContainers, 0);
+        return lastIndex;
+      }
+    });
+  };
+  
+  const handleRightButtonClick = () => {
+    setCurrentIndex((prevIndex) => {
+      const lastIndex = (previousDrops?.length || 1) - numContainers;
+      if (prevIndex < lastIndex) {
+        return prevIndex + 1;
+      } else {
+        // If at the end of the array, move to the beginning
+        return 0;
+      }
+    });
+  };
+
+  const addProductToCart = (product: Product) => {
+    // Update the localCartItems array
+    cartContext?.setLocalCartItems((prevItems) => [...prevItems, product]);
+
+    // Update the cartSubTotal
+    cartContext?.setCartSubTotal((prevSubTotal) => prevSubTotal + product.unitPrice);
+  };
+
+  const addToCart = (product: Product) => {
+    
+    if(cartContext?.localCartItems.length == 0) {
+      addProductToCart(product);
+      toast.success('Added To Cart!')
+    } else {
+      let a = false
+      for(let i = 0; i < cartContext?.localCartItems.length!; i++) {
+        
+        if(product.id == cartContext?.localCartItems[i].id) {
+          console.log(`The product id = ${product.id} and the cartItemId = ${cartContext?.localCartItems[i].id}`)
+          a = true
+          
+        }
+      }
+        if(a === false) {
+
+          addProductToCart(product);
+          toast.success('Added To Cart!')
+        } else {
+
+          toast.error ('Product Already in Cart')
+        }
+        
+    }
+  };
+  
+
+
+
+
+  
   return (
     <div className='previousdrops'>
       <div className='previousdrops-text-container'>
@@ -9,42 +131,29 @@ const PreviousDrops = () => {
         <h6>Get them before they're gone!</h6>
       </div>
       <div className='previousdrops-carousel'>
+
+
+
+
+
         <div className='previousdrops-carousel-slides-container'>
+          {previousDrops?.slice(currentIndex, currentIndex + numContainers).map((product, index) => {
+            return (
           <div className='previousdrops-carousel-slide1-container'>
-            <div className="previousdrops-carousel-slide1-image-container"></div>
+            <Link to={`/products/${product.id}`} className="previousdrops-carousel-slide1-image-container"><img width="75%" height='75%' src={product.imageUrl}/></Link>
             <div className="previousdrops-carousel-slide1-title-button-remaining-container">
-              <h6>FRUIT ON FURNITURE</h6>
-              <button>SOLD OUT</button>
-              <p>0 of 100 REMAINING</p>
+              <h4>{product.name}</h4>
+              {product.active === true ? <button style={{cursor: 'pointer'}} onClick={() => {addToCart(product)}}>PURCHASE</button> : <button>SOLD OUT</button>}
+              <p>In Stock: {product.quantity} of 100</p>
             </div>
           </div>
-          <div className='previousdrops-carousel-slide2-container'>
-            <div className="previousdrops-carousel-slide2-image-container"></div>
-            <div className="previousdrops-carousel-slide2-title-button-remaining-container">
-              <h6>FRUIT ON FURNITURE</h6>
-              <button>SOLD OUT</button>
-              <p>0 of 100 REMAINING</p>
-            </div>
-          </div>
-          <div className='previousdrops-carousel-slide3-container'>
-            <div className="previousdrops-carousel-slide3-image-container">
-               
-            </div>
-            <div className="previousdrops-carousel-slide3-title-button-remaining-container">
-              <h6>FRUIT ON FURNITURE</h6>
-              <button>SOLD OUT</button>
-              <p>0 of 100 REMAINING</p>
-            </div>
-          </div>
+          )
+        })}
         </div>
         <div className='previousdrops-carousel-nav-container'>
-          <div className='previousdrops-carousel-nav-line-container'>
-            <div className='previousdrops-carousel-nav-line'></div>
-            <div className='previousdrops-carousel-nav-progress-line'></div>
-          </div>
           <div className='previousdrops-carousel-nav-button-container'>
-            <button id='previousdrops-carousel-nav-button-left'>&#60;</button>
-            <button id='previousdrops-carousel-nav-button-right'>&#62;</button>
+            <button onClick={handleLeftButtonClick} id='previousdrops-carousel-nav-button-left'>&#60;</button>
+            <button onClick={handleRightButtonClick} id='previousdrops-carousel-nav-button-right'>&#62;</button>
           </div>
         </div>
       </div>
